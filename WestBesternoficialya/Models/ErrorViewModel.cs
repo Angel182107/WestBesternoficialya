b11790b6e06ca3178e66192056a60e369afbb11a
Uriel_Dev <angelurielmonarca@gmail.com>
2026-05-02 11:02:12 -0600

    Add project files.

---
+++ WestBesternoficialya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WestBesternoficialya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
