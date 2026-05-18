b8f42e3288f21eff9e51941b627b2b36b7ec0c9d
Marcuz2000 <Marcuz3179@gmail.com>
2026-05-18 14:54:31 -0600

    lol x2

--- WestBesternoficialya/Controllers/AccesoController.cs
+++ WestBesternoficialya/Controllers/AccesoController.cs
@@ -5,19 +5,16 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace WestBesternoficialya.Controllers;
 
-// GPS 1: Le decimos que esta clase entera responde a la palabra "Acceso"
 [Route("Acceso")]
 public class AccesoController : Controller
 {
-    // GPS 2: Le decimos que esta pantalla responde a la palabra "Index"
     [Route("Index")]
-    [Route("")] // Por si acaso alguien escribe solo "/Acceso"
+    [Route("")]
     public IActionResult Index()
     {
         return View("~/Views/Acceso/Index.cshtml");
     }
 
-    // GPS 3: Ruta forzada para el botón de entrar
     [HttpPost("Entrar")]
     public async Task<IActionResult> Entrar(string correo, string clave)
     {
@@ -32,7 +29,6 @@ public class AccesoController : Controller
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
 
-            // Si entra, lo mandamos al inicio
             return RedirectToAction("Index", "Home");
         }
 
@@ -40,7 +36,6 @@ public class AccesoController : Controller
         return View("~/Views/Acceso/Index.cshtml");
     }
 
-    // GPS 4: Ruta para cerrar sesión
     [Route("Salir")]
     public async Task<IActionResult> Salir()
     {
